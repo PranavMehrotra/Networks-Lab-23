fc2302c048a8e517fbb8ac232ee9e4f91db09a36
PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
2023-03-13 01:23:41 +0530

    Almost Done! Locks left

--- Assignments/A5/mysocket.c
+++ Assignments/A5/mysocket.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h> 
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <time.h>
-#include <sys/stat.h>
-#include<errno.h>
 #include "mysocket.h"
 
+// Global variables of queue
+queue_head *send_queue;
+queue_head *receive_queue;
+int my_type=0;
+int curr_sockfd=-1;
+pthread_t R_thread;
+pthread_t S_thread;
+pthread_attr_t R_attr;
+pthread_attr_t S_attr;
 
 int my_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
 {
@@ -22,55 +20,274 @@ int my_listen(int sockfd, int backlog)
     return listen(sockfd, backlog);
 }
 
-int my_close(int fd)
-{
-    //clean send receieve table
-    //delete send receiev table
-    return close(fd);
-}
-
 int my_connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
 {
+    curr_sockfd = sockfd;
     return connect(sockfd,addr,addrlen);
 }
 
 int my_accept(int sockfd, struct sockaddr *restrict addr, socklen_t *restrict addrlen)
 {
-    return accept(sockfd, addr, addrlen);
+    return curr_sockfd = accept(sockfd, addr, addrlen);
 }
 
-ssize_t my_send(int sockfd, const void *buf, size_t len, int flags)
-{
 
+// Helper functions
+int recv_num(int newsockfd){
+    char num[10];
+    int ans;
+    int y,len=0,siz=sizeof(int);
+    while((siz>len) && ((y=recv(newsockfd,num+len,siz-len,0))>0)){
+        len+=y;
+        if(len==siz){
+            memcpy(&ans,num,siz);
+            printf("Recieved size = %d\n",ans);
+            break;
+        }
+    }
+    return ans;
 }
-ssize_t my_recv(int sockfd, void *buf, size_t len, int flags)
-{
+
+int min(int a,int b){
+    return (a<b)?a:b;
+}
+
+//receive an expression/string in chunks
+char *recieve_expr(int newsockfd, int *len_recieved){
+	char *s;
+	int len=0,size = 2*MAX_CHUNK_SIZE,y,chunk=MAX_CHUNK_SIZE,recv_size = recv_num(newsockfd);
+    s = (char *)malloc(sizeof(char)*size);
+	if(!s)	return NULL;
+    //keep on receiving characters in chunks and store them in buffer
+	while((recv_size > len) && ((y=recv(newsockfd,s+len,min(recv_size-len,chunk),0))>0)){
+        len+=y;
+		//update length of message recieved
+		while(len+chunk>=size){
+            //realloc the array to accomodate more incoming messages
+			s = realloc(s,sizeof(char)*(size*=2));
+			if(!s)	return NULL;
+		}
+	}
+	if(len==0){
+        //No character recieved i.e recv returned <=0
+		free(s);
+        *len_recieved = 0;
+		return NULL;
+	}
+	s = realloc(s,sizeof(char)*(len+1));
+    *len_recieved = len;
+    return s;
+    //return the message read and update the length
+}
+
+//send expression in chunks
+void send_expr(int newsockfd, char *expr, int size){
+	int len=0,y,chunk=MAX_CHUNK_SIZE;
+    // Send the size of the message
+    send(newsockfd,&size,sizeof(int),0);
+	if(size<chunk){
+        //if the size to be sent is less than chunk size send the entire message
+		send(newsockfd,expr,size,0);
+		return;
+	}
+	while(1){
+        //else break in chunks of size MAX_SIZE
+		if(len+chunk>size){
+			send(newsockfd,expr+len,size-len,0);
+			break;
+		}
+		y=send(newsockfd,expr+len,chunk,0);
+		len+=y;
+	}
+}
+
+
+// Push to the queue
+void push(queue_head *head, char *s, int len){
+    queue *new_node = (queue *)malloc(sizeof(queue));
+    new_node->data = s;
+    new_node->len = len;
+    new_node->next = NULL;
     
+    head->rear->next = new_node;
+    head->rear = new_node;
 }
 
-//r wait on recv
-//receive data via multiple call
-//interpret and form message
-//put in receieve table
+// Pop from the queue
+char *pop(queue_head *head, int *len){
+    if(head->front == head->rear)  return NULL;
+    queue *temp = head->front->next;
+    char *s = temp->data;
+    *len = temp->len;
+    head->front->next = temp->next;
+    if(head->rear == temp)  head->rear = head->front;
+    free(temp);
+    return s;
+}
+
+// Cleanup function for R
+void cleanup_R(void *arg){
+    printf("Cleaning R\n");
+    // Free the Receive queue
+    queue *temp = receive_queue->front->next;
+    while(temp){
+        queue *temp1 = temp->next;
+        free(temp->data);
+        free(temp);
+        temp = temp1;
+    }
+    free(receive_queue->front);
+    free(receive_queue);
+}
+
+// Cleanup function for S
+void cleanup_S(void *arg){
+    printf("Cleaning S\n");
+    // Free the Send queue
+    queue *temp = send_queue->front->next;
+    while(temp){
+        queue *temp1 = temp->next;
+        free(temp->data);
+        free(temp);
+        temp = temp1;
+    }
+    free(send_queue->front);
+    free(send_queue);
+}
+
+// R Thread to wait on recv() call and put the message in the receive table
+void *R(void *arg)
+{
+    // Register the cleanup function
+    pthread_cleanup_push(cleanup_R, NULL);
+    // Poll Structure
+    struct pollfd fdset[1];
+    int timeout=POLL_TIMEOUT,ret,temp_sockfd;
+    while(1){
+        temp_sockfd = curr_sockfd;
+        if(temp_sockfd==-1){
+            // Test Cancel
+            pthread_testcancel();
+            continue;
+        }
+        fdset[0].fd = temp_sockfd;
+        fdset[0].events = POLLIN;
+        // Poll for message with timeout 1 sec
+        ret = poll(fdset,1,timeout);
+		if(ret<0){
+			perror("Poll error. Please restart the load balancer.\n");
+			cleanup_R(NULL);
+            pthread_exit(0);
+		}
+        if(ret==0){
+            // Timeout
+            // Test Cancel
+            pthread_testcancel();
+            continue;
+        }
+        int len=0;
+        char *s = recieve_expr(temp_sockfd, &len);
+        if(len==0)  continue;
+        //put in receive table
+        push(receive_queue, s, len);
+    }
+    pthread_cleanup_pop(1);
+    pthread_exit(0);
+}
 
 //s sleep for t time
 //wake
 //see if message pending
 //send in one or more call
+void *S(void *arg)
+{
+    // Register the cleanup function
+    pthread_cleanup_push(cleanup_S, NULL);
+    while(1){
+        //sleep for t time
+        sleep(SLEEP_TIME);
+        // Test Cancel
+        pthread_testcancel();
+        //wake
+        //see if message pending
+        int len=0;
+        char *s = pop(send_queue, &len);
+        if(len==0 || s==NULL)  continue;
+        //send in one or more call
+        send_expr(curr_sockfd, s, len);
+        free(s);
+    }
+    pthread_cleanup_pop(1);
+    pthread_exit(0);
+}
 
 
 int my_socket(int domain, int type, int protocol)
 {
     int sockfd;
-    if(domain == SOCK_MyTCP)sockfd  = socket(AF_INET, type, protocol);
-    else sockfd = socket(domain, type, protocol);
+    if(type == SOCK_MyTCP)
+    {
+        my_type = 1;
+        sockfd  = socket(AF_INET, SOCK_STREAM, 0);
+    }
+    else return socket(domain, type, protocol);
 
-    //create R and S
     //Initialise send recieve table
+    //create R and S
+    if(pthread_attr_init(&R_attr) < 0){
+        exit_with_error("userSimulator::pthread_attr_init() failed");
+    }
+    if(pthread_create(&R_thread , &R_attr , R , NULL) < 0){
+        exit_with_error("userSimulator::pthread_create() failed");
+    }
+    if(pthread_attr_init(&S_attr) < 0){
+        exit_with_error("userSimulator::pthread_attr_init() failed");
+    }
+    if(pthread_create(&S_thread , &S_attr , S , NULL) < 0){
+        exit_with_error("userSimulator::pthread_create() failed");
+    }
 
     return sockfd;
 }
-int main(void)
+
+int my_close(int sockfd)
 {
-    printf("%d", SOCK_MyTCP);
+    if(my_type){
+        if(curr_sockfd == sockfd){
+            curr_sockfd = -1;
+        }
+        else{
+            my_type = 0;
+            curr_sockfd = -1;
+            //kill R and S threads
+            pthread_cancel(R_thread);
+            pthread_cancel(S_thread);
+            
+        }
+    }
+    return close(sockfd);
+}
+
+ssize_t my_send(int sockfd, const void *buf, size_t len, int flags){
+    if(my_type){
+        //put in send table
+        if(len<=0) return 0;
+        char *s = (char *)malloc(sizeof(char)*len);
+        memcpy(s, buf, len);
+        push(send_queue, s, len);
+        return len;
+    }
+    else return send(sockfd, buf, len, flags);
+}
+ssize_t my_recv(int sockfd, void *buf, size_t len, int flags){
+    if(my_type){
+        //put in receive table
+        int len=0;
+        char *s = pop(receive_queue, &len);
+        if(len==0 || s==NULL)  return 0;
+        memcpy(buf, s, len);
+        free(s);
+        return len;
+    }
+    else return recv(sockfd, buf, len, flags);
 }
--- Assignments/A5/mysocket.h
+++ Assignments/A5/mysocket.h
@@ -1,4 +1,48 @@
-#define SOCK_MyTCP 1
+#ifndef MYSOCKET_H
+#define MYSOCKET_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h> 
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <pthread.h>
+#include <poll.h>
+
+#define SOCK_MyTCP 128
+#define MAX_MSG_SIZE 5000
+#define MAX_CHUNK_SIZE 1000
+#define SLEEP_TIME 1
+#define POLL_TIMEOUT 1000
+
+// Structure for storing a linked list with front and rear pointers for the send and receive queues of a socket
+typedef struct _queue
+{
+    struct _queue *next;
+    char *data;
+    int len;
+}queue;
+
+// Structure for storing the front and rear pointers of the send and receive queues of a socket
+typedef struct _queue_head
+{
+    queue *front;
+    queue *rear;
+    /* 
+    If front == rear, then the queue is empty
+    One dummy node is always present in the queue at the beginning, so front and rear will never be NULL
+    If you want to add a node to the queue, add it after the rear node and then move the rear pointer to the new node
+    If you want to remove a node from the queue, remove the node at the front->next and then update the front->next to the new node
+    Also if front->next == rear, then the queue has only one node, so after removing the node, set front->next = NULL and rear = front
+    */
+}queue_head;
+
 
 
 int my_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
@@ -9,3 +53,5 @@ int my_listen(int sockfd, int backlog);
 int my_close(int fd);
 ssize_t my_send(int sockfd, const void *buf, size_t len, int flags);
 ssize_t my_recv(int sockfd, void *buf, size_t len, int flags);
+
+#endif
