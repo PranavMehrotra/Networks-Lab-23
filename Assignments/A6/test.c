acbe0512c2cd7706963893d2254f059a463c9a05
PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
2023-04-01 14:54:04 +0530

    traceroute works

--- Assignments/A6/test.c
+++ Assignments/A6/test.c
@@ -12,6 +12,30 @@
 
 #define MAX_SIZE 4096
 
+void print_ip_header(unsigned char *buf){
+    struct iphdr *ip_hdr = (struct iphdr *)buf;
+    printf("IP header:\n");
+    printf("  Check: %d\n", ip_hdr->check);
+    printf("  Version: %d\n", ip_hdr->version);
+    printf("  Source: %s\n", inet_ntoa(*(struct in_addr *)&ip_hdr->saddr));
+    printf("  Destination: %s\n", inet_ntoa(*(struct in_addr *)&ip_hdr->daddr));
+    printf("  Header length: %d\n", ip_hdr->ihl);
+    printf("  Total length: %d\n", ip_hdr->tot_len);
+    printf("  TTL: %d\n", ip_hdr->ttl);
+    printf("  ID: %d\n", ip_hdr->id);
+    printf("  Protocol: %d\n", ip_hdr->protocol);
+    printf("  Fragment offset: %d\n", ip_hdr->frag_off);
+}
+
+void print_icmp_header(unsigned char *buf){
+    struct icmphdr *icmp_hdr = (struct icmphdr *)(buf + sizeof(struct iphdr));
+    printf("ICMP header:\n");
+    printf("  Type: %d\n", icmp_hdr->type);
+    printf("  Code: %d\n", icmp_hdr->code);
+    printf("  Checksum: %d\n", icmp_hdr->checksum);
+}
+
+
 int main(){
     int			sockfd ;
 	struct sockaddr_in	serv_addr;
@@ -30,54 +54,57 @@ int main(){
     while(1){
         int serv_len = sizeof(serv_addr);
         n = recvfrom(sockfd, buf, MAX_SIZE, 0, (struct sockaddr *)&serv_addr, &serv_len);
-        printf("Server IP: %s\n", inet_ntoa(serv_addr.sin_addr));
-        printf("Server Port: %d\n\n", ntohs(serv_addr.sin_port));
-        printf("IP header:\n");
-        printf("  Check: %d\n", ip_hdr->check);
-        printf("  Version: %d\n", ip_hdr->version);
-        printf("  Source: %s\n", inet_ntoa(*(struct in_addr *)&ip_hdr->saddr));
-        printf("  Destination: %s\n", inet_ntoa(*(struct in_addr *)&ip_hdr->daddr));
-        printf("  Header length: %d\n", ip_hdr->ihl);
-        printf("  Total length: %d\n", ip_hdr->tot_len);
-        printf("  TTL: %d\n", ip_hdr->ttl);
-        printf("  ID: %d\n", ip_hdr->id);
-        printf("  Protocol: %d\n", ip_hdr->protocol);
-        printf("  Fragment offset: %d\n", ip_hdr->frag_off);
+        // printf("Server IP: %s\n", inet_ntoa(serv_addr.sin_addr));
+        // printf("Server Port: %d\n\n", ntohs(serv_addr.sin_port));
+        // printf("IP header:\n");
+        // printf("  Check: %d\n", ip_hdr->check);
+        // printf("  Version: %d\n", ip_hdr->version);
+        // printf("  Source: %s\n", inet_ntoa(*(struct in_addr *)&ip_hdr->saddr));
+        // printf("  Destination: %s\n", inet_ntoa(*(struct in_addr *)&ip_hdr->daddr));
+        // printf("  Header length: %d\n", ip_hdr->ihl);
+        // printf("  Total length: %d\n", ip_hdr->tot_len);
+        // printf("  TTL: %d\n", ip_hdr->ttl);
+        // printf("  ID: %d\n", ip_hdr->id);
+        // printf("  Protocol: %d\n", ip_hdr->protocol);
+        // printf("  Fragment offset: %d\n", ip_hdr->frag_off);
 
 
-        printf("ICMP header:\n");
-        printf("  Type: %d\n", icmp_hdr->type);
-        printf("  Code: %d\n", icmp_hdr->code);
+        // printf("ICMP header:\n");
+        // printf("  Type: %d\n", icmp_hdr->type);
+        // printf("  Code: %d\n", icmp_hdr->code);
 
-        // printf("Packet:\n");
-        // for (int i = (sizeof(struct iphdr) + sizeof(struct icmphdr)); i < n; i++) {
-        //     printf("%c ", (unsigned char)buf[i]);
-        // }
+        print_ip_header(buf);
+        print_icmp_header(buf);
+
         printf("Payload:\n");
-
-        char *src_ptr = buf + sizeof(struct iphdr) + sizeof(struct icmphdr);
-        size_t src_len = n - sizeof(struct iphdr) - sizeof(struct icmphdr);
-        char dest_buf[MAX_SIZE];
-        char *dest_ptr = dest_buf;
-        size_t dest_len = MAX_SIZE;
-
-        iconv_t conv = iconv_open("UTF-8", "ISO-8859-1");
-        if (conv == (iconv_t)-1) {
-            perror("iconv_open");
-            exit(EXIT_FAILURE);
+        for (int i = (sizeof(struct iphdr) + sizeof(struct icmphdr)); i < n; i++) {
+            printf("%c ", (unsigned char)buf[i]);
         }
+        // printf("Payload:\n");
 
-        if (iconv(conv, &src_ptr, &src_len, &dest_ptr, &dest_len) == (size_t)-1) {
-            perror("iconv");
-            exit(EXIT_FAILURE);
-        }
+        // char *src_ptr = buf + sizeof(struct iphdr) + sizeof(struct icmphdr);
+        // size_t src_len = n - sizeof(struct iphdr) - sizeof(struct icmphdr);
+        // char dest_buf[MAX_SIZE];
+        // char *dest_ptr = dest_buf;
+        // size_t dest_len = MAX_SIZE;
 
-        *dest_ptr = '\0';
+        // iconv_t conv = iconv_open("UTF-8", "ISO-8859-1");
+        // if (conv == (iconv_t)-1) {
+        //     perror("iconv_open");
+        //     exit(EXIT_FAILURE);
+        // }
 
-        printf("%s\n\n", dest_buf);
+        // if (iconv(conv, &src_ptr, &src_len, &dest_ptr, &dest_len) == (size_t)-1) {
+        //     perror("iconv");
+        //     exit(EXIT_FAILURE);
+        // }
 
-        iconv_close(conv);
-        // printf("\n\n");
+        // *dest_ptr = '\0';
+
+        // printf("%s\n\n", dest_buf);
+
+        // iconv_close(conv);
+        printf("\n\n");
         // printf("\n\n\n");
 
     }
--- Assignments/A6/traceroute.c
+++ Assignments/A6/traceroute.c
@@ -11,12 +11,13 @@
 #include <netdb.h>
 
 #define PACKET_SIZE 64
-#define MAX_HOPS 30
+#define MAX_HOPS 10
 
 unsigned short in_cksum(unsigned short *addr, int len) {
     unsigned int sum = 0;
     while (len > 1) {
-        sum += *addr++;
+        sum += *addr;
+        addr++;
         len -= 2;
     }
     if (len == 1) {
@@ -26,6 +27,30 @@ unsigned short in_cksum(unsigned short *addr, int len) {
     sum += (sum >> 16);
     return (unsigned short)(~sum);
 }
+
+void print_ip_header(unsigned char *buf){
+    struct iphdr *ip_hdr = (struct iphdr *)buf;
+    printf("IP header:\n");
+    printf("  Check: %d\n", ip_hdr->check);
+    printf("  Version: %d\n", ip_hdr->version);
+    printf("  Source: %s\n", inet_ntoa(*(struct in_addr *)&ip_hdr->saddr));
+    printf("  Destination: %s\n", inet_ntoa(*(struct in_addr *)&ip_hdr->daddr));
+    printf("  Header length: %d\n", ip_hdr->ihl);
+    printf("  Total length: %d\n", ip_hdr->tot_len);
+    printf("  TTL: %d\n", ip_hdr->ttl);
+    printf("  ID: %d\n", ip_hdr->id);
+    printf("  Protocol: %d\n", ip_hdr->protocol);
+    printf("  Fragment offset: %d\n", ip_hdr->frag_off);
+}
+
+void print_icmp_header(unsigned char *buf){
+    struct icmphdr *icmp_hdr = (struct icmphdr *)(buf + sizeof(struct iphdr));
+    printf("ICMP header:\n");
+    printf("  Type: %d\n", icmp_hdr->type);
+    printf("  Code: %d\n", icmp_hdr->code);
+    printf("  Checksum: %d\n", icmp_hdr->checksum);
+}
+
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -56,14 +81,14 @@ int main(int argc, char *argv[]) {
     
     // Set the TTL value for the socket
     int ttl = 1;
-    if (setsockopt(sockfd, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl)) != 0) {
-        printf("Error setting TTL value: %s\n", strerror(errno));
-        return 1;
-    }
+    // if (setsockopt(sockfd, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl)) != 0) {
+    //     printf("Error setting TTL value: %s\n", strerror(errno));
+    //     return 1;
+    // }
     
     // Set the socket timeout to 1 second
     struct timeval timeout;
-    timeout.tv_sec = 3;
+    timeout.tv_sec = 1;
     timeout.tv_usec = 0;
     if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) != 0) {
         printf("Error setting socket timeout: %s\n", strerror(errno));
@@ -81,9 +106,11 @@ int main(int argc, char *argv[]) {
     struct icmphdr *icmp_packet = (struct icmphdr *)packet;
     icmp_packet->type = ICMP_ECHO;
     icmp_packet->code = 0;
-    
     int seq = 0;
-    
+    // packet[sizeof(struct icmphdr)] = 'E';
+    // packet[sizeof(struct icmphdr) + 1] = 'C';
+    // packet[sizeof(struct icmphdr) + 2] = 'E';
+    // packet[sizeof(struct icmphdr) + 3] = '\0';
     // Loop through the TTL values
     for (ttl = 1; ttl <= MAX_HOPS; ttl++) {
         // Set the TTL value for the socket
@@ -92,38 +119,39 @@ int main(int argc, char *argv[]) {
             return 1;
         }
         
-        // Initialize the source address structure
-        struct sockaddr_in src_addr;
-        memset(&src_addr, 0, sizeof(src_addr));
-        src_addr.sin_family = AF_INET;
-        src_addr.sin_addr.s_addr = INADDR_ANY;
+        // // Initialize the source address structure
+        // struct sockaddr_in src_addr;
+        // memset(&src_addr, 0, sizeof(src_addr));
+        // src_addr.sin_family = AF_INET;
+        // src_addr.sin_addr.s_addr = INADDR_ANY;
         
-        // Bind the socket to the source address
-        if (bind(sockfd, (struct sockaddr *)&src_addr, sizeof(src_addr)) != 0) {
-            printf("Error binding socket: %s\n", strerror(errno));
-            return 1;
-        }
+        // // Bind the socket to the source address
+        // if (bind(sockfd, (struct sockaddr *)&src_addr, sizeof(src_addr)) != 0) {
+        //     printf("Error binding socket: %s\n", strerror(errno));
+        //     return 1;
+        // }
         
         // Set the ICMP sequence number
         // icmp_packet->icmp_seq = seq++;
         
         // Calculate the checksum for the ICMP packet
         icmp_packet->checksum = 0;
-        icmp_packet->checksum = htons(~(htons(in_cksum((unsigned short *)icmp_packet, PACKET_SIZE))));
+        icmp_packet->checksum = in_cksum((unsigned short *)icmp_packet, sizeof(struct icmphdr));
         
         printf("%d ", ttl);
 
         // Send the ICMP packet to the destination
-        if (sendto(sockfd, packet, PACKET_SIZE, 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr)) <= 0) {
+        if (sendto(sockfd, packet, sizeof(struct icmphdr), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr)) <= 0) {
             printf("Error sending packet: %s\n", strerror(errno));
             return 1;
         }
         
         // Receive the response from the router
-        char recv_buf[PACKET_SIZE];
+        char recv_buf[PACKET_SIZE+1];
         struct sockaddr_in recv_addr;
         socklen_t recv_addr_len = sizeof(recv_addr);
-        if (recvfrom(sockfd, recv_buf, PACKET_SIZE, 0, (struct sockaddr *)&recv_addr, &recv_addr_len) <= 0) {
+        int recv_len;
+        if ((recv_len = recvfrom(sockfd, recv_buf, PACKET_SIZE, 0, (struct sockaddr *)&recv_addr, &recv_addr_len)) <= 0) {
             printf("**\n");
             continue;
         }
@@ -139,13 +167,16 @@ int main(int argc, char *argv[]) {
         // Check if the response is an ICMP echo reply
         struct iphdr *ip_header = (struct iphdr *)recv_buf;
         struct icmphdr *icmp_reply = (struct icmphdr *)(recv_buf + (ip_header->ihl * 4));
+        // printf("\n");
+        // print_ip_header(recv_buf);
+        // print_icmp_header(recv_buf);
+        // printf("\n\n");
         if (icmp_reply->type == ICMP_ECHOREPLY) {
             printf("Done\n");
             break;
         }
-        printf("Source addr: %d ",ip_header->saddr);
         if(ip_header->saddr == dest_ip.s_addr){
-            printf("Done\n");
+            printf("Done2\n");
             break;
         }
         
